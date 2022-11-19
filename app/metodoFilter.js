3ecc82700be195a54708c6274b1070df3d6932af
Gustavo Candotti <82784764+GustavoCandotti@users.noreply.github.com>
2022-11-19 13:49:42 -0300

    metodos array para js

---
+++ app/metodoFilter.js
@@ -0,0 +1,32 @@
+const botoes = document.querySelectorAll('.btn')  //selecionamos todos os botoes.
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)  //a cada clique, pegue o id do botao.
+    const categoria = elementoBtn.value //buscar o value, back end ou front end ou dados.
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
+}
+
+// ? = if
+// : = else
